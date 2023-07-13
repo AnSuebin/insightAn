c91cf48f2f330bc7072b90b8342f244e58cf1910
Suebin An <jejupalette@naver.com>
2023-07-13 20:42:36 +0900

    styles : footer 배치 조정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-end">
+    <footer className="flex flex-col">
       <div className="text-right">
         다음을 위해 고민하고
         <br />
         깨달음을 얻어
         <br />한 발짝씩 나아 가는 개발자
       </div>
-      <div>프론트엔드 개발자 안수빈</div>
+      <div className="text-right">프론트엔드 개발자 안수빈</div>
     </footer>
   );
 };
