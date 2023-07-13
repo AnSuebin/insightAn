d18fb4e6e1316bdab489081083a240f127d18afc
Suebin An <jejupalette@naver.com>
2023-07-13 20:38:44 +0900

    feat : layout header 제작

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center">
+      <Link href="/">
+        <div>
+          Make insights
+          <br />
+          for the Next
+        </div>
+      </Link>
+      <nav>
+        <Link href="/career">
+          <div>career</div>
+        </Link>
+        <Link href="/contact">
+          <div>contact</div>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
