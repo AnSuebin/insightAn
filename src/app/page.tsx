a0237498bcc2b31553e75da24410575a36b56ac9
Suebin An <jejupalette@naver.com>
2023-07-13 23:47:21 +0900

    styles : 첫 페이지 layout 디자인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import MaxWidth from "@/components/layout/MaxWidth";
+
 export default function Home() {
-  return <section>an blog</section>;
+  return (
+    <section className="w-full h-fullScreenExceptHeader relative">
+      <MaxWidth>
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="text-9xl font-extrabold text-mainColor mb-10">
+            Insight
+          </div>
+        </div>
+      </MaxWidth>
+    </section>
+  );
 }
