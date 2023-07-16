b73dac3941d79c5cb3088704d10e8a244367b1a2
Suebin An <jejupalette@naver.com>
2023-07-16 15:56:35 +0900

    feat : home portfolio 데이터 연결

---
+++ src/apis/home/home.ts
@@ -0,0 +1,8 @@
+import path from "path";
+import { readFile } from "fs/promises";
+import { IHomePortfolio } from "./home.type";
+
+export const getHomePortfolios = async (): Promise<IHomePortfolio[]> => {
+  const filePath = path.join(process.cwd(), "data", "portfolioList.json");
+  return readFile(filePath, "utf-8").then<IHomePortfolio[]>(JSON.parse);
+};
--- src/components/pages/home/HomePortfolio.tsx
+++ src/components/pages/home/HomePortfolio.tsx
@@ -2,30 +2,20 @@ import React from "react";
 import HomePortfolioCard from "./HomePortfolioCard";
 import BottomArrow from "@/components/common/BottomArrow";
 import Link from "next/link";
+import { getHomePortfolios } from "@/apis/home/home";
 
-const HomePortfolio = () => {
+const HomePortfolio = async () => {
+  const portfolioListData = await getHomePortfolios();
+
   return (
     <div className="relative w-full h-screen flex justify-center items-center gap-16">
-      <div className="-translate-y-20">
-        <Link href="/portfolio">
-          <HomePortfolioCard />
-        </Link>
-      </div>
-      <div className="translate-y-20">
-        <Link href="/portfolio">
-          <HomePortfolioCard />
-        </Link>
-      </div>
-      <div className="-translate-y-20">
-        <Link href="/portfolio">
-          <HomePortfolioCard />
-        </Link>
-      </div>
-      <div className="translate-y-20">
-        <Link href="/portfolio">
-          <HomePortfolioCard />
-        </Link>
-      </div>
+      {portfolioListData.map((portfolio) => (
+        <div className="-translate-y-20">
+          <Link href="/portfolio">
+            <HomePortfolioCard item={portfolio} />
+          </Link>
+        </div>
+      ))}
       <BottomArrow />
     </div>
   );
