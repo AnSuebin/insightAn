944b6b4c13a9d564f18f9c272f2b69c1d3406c90
Suebin An <jejupalette@naver.com>
2023-07-16 10:54:00 +0900

    feat : layout maxwidth 컴포넌트 두가지로 분리

---
+++ src/components/layout/FullHightMaxWidth.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const FullHightMaxWidth: FC<Props> = ({ children }) => {
+  return (
+    <div className="flex w-full h-full max-w-screen-2xl m-auto">{children}</div>
+  );
+};
+
+export default FullHightMaxWidth;
