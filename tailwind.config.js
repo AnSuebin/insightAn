66cd41069141ffd306de84f9724387b39ed7c575
Suebin An <jejupalette@naver.com>
2023-07-16 14:18:12 +0900

    feat : 포트폴리오 페이지 디자인 제작

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,12 @@ module.exports = {
       width: {
         imageWidth: "100%",
       },
+      flex: {
+        2: "2 2 0%",
+      },
+      spacing: {
+        "120px": "120px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
