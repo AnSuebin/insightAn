a51568c8c636f58afcf0535fe034a828697018f6
Suebin An <jejupalette@naver.com>
2023-07-16 10:54:54 +0900

    styles : 두번째 페이지 디자인 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
+import FullHightMaxWidth from "@/components/layout/FullHightMaxWidth";
 import MaxWidth from "@/components/layout/MaxWidth";
+import ArrowIcon from "@/icons/arrowIcon";
 
 export default function Home() {
   return (
-    <section className="w-full h-fullScreenExceptHeader relative">
+    <section className="w-full">
       <MaxWidth>
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center items-center">
           <div className="text-9xl font-extrabold text-mainColor mb-10">
             Insight
           </div>
         </div>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+          <div>
+            <div className="flex gap-5 items-center">
+              <div className="text-8xl font-extrabold text-mainColor mb-5">
+                Make Insight <br />
+                for the Next.
+              </div>
+              <ArrowIcon />
+            </div>
+            <p className="text-3xl font-normal text-mainColor mb-10">
+              다음을 위해 고민하고, 깨달음을 얻어 한 발짝씩 나아 가는 개발자
+            </p>
+          </div>
+        </div>
+        <div className="w-full h-screen flex flex-col justify-center items-center"></div>
       </MaxWidth>
     </section>
   );
